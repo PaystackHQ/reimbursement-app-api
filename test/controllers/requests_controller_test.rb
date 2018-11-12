e572d3a6eb9abcf20a7ca351059aedee70f0b4de
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2018-11-12 23:07:05 +0100

    Install rspec and other test dependencies

--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RequestsControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::RequestsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
