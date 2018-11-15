444b827fbf77069bfaa3f21224622c4a7b44726d
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2018-11-16 00:42:49 +0100

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      devise_for :users
       resources :requests
       post '/requests/:id/approve', to: 'requests#approve'
       post '/requests/:id/reject', to: 'requests#reject'
